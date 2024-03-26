70e31572ffa8dd38f337cb31c36f9db2c761a440
Nishit-Sharma <nishit.sharma@mcvts.org>
2024-03-26 09:57:53 -0400

    Very basic skeleton

--- src/AllRoutes.js
+++ src/AllRoutes.js
@@ -4,6 +4,15 @@ import React from 'react';
 import Home from "./components/Home.js";
 
 import Place1 from "./components/towns/place1/Place.js";
+import Place2 from "./components/towns/place2/Place.js";
+import Place3 from "./components/towns/place3/Place.js";
+import Place4 from "./components/towns/place4/Place.js";
+import Place5 from "./components/towns/place5/Place.js";
+import Place6 from "./components/towns/place6/Place.js";
+import Place7 from "./components/towns/place7/Place.js";
+import Place8 from "./components/towns/place8/Place.js";
+import Place9 from "./components/towns/place9/Place.js";
+import Place10 from "./components/towns/place10/Place.js";
 
 // Importing Router Dom which allows us to link to different pages
 import { Route, Routes, useLocation} from 'react-router-dom';
@@ -19,6 +28,15 @@ function AllRoutes() {
 
             {/* Creating routes to all of the other pages. it is named this way so we can use a for loop to link to each product */}
             <Route path="place1" element={<Place1 />} />
+            <Route path="place2" element={<Place2 />} />
+            <Route path="place3" element={<Place3 />} />
+            <Route path="place4" element={<Place4 />} />
+            <Route path="place5" element={<Place5 />} />
+            <Route path="place6" element={<Place6 />} />
+            <Route path="place7" element={<Place7 />} />
+            <Route path="place8" element={<Place8 />} />
+            <Route path="place9" element={<Place9 />} />
+            <Route path="place10" element={<Place10 />} />
 
         </Routes>
     );
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,24 +5,17 @@ import './Global.css';
 
 function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Link to="/place1">
-            <button>button</button>
-        </Link>
-      </header>
+    <div className="Home">
+      <h1>Welcome to our travel website!</h1>
+      <p>Choose from one of our 10 places to learn more:</p>
+      <div className="places-container">
+        {[...Array(10)].map((_, index) => (
+          <Link key={index} to={`/place${index + 1}`} className="place-link">
+            <img src={`place${index + 1}.jpg`} alt={`Place ${index + 1}`} className="place-image" />
+            <p>Place {index + 1}</p>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
