7e881b3edf5d932967afafa6db199efdd15a282d
Nishit-Sharma <nishit.sharma@mcvts.org>
2024-03-27 12:41:55 -0400

    Commented out an import

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSpring, animated } from 'react-spring';
 import Layout from './Layout.js';
-import { Box, Center, Image, Flex, Badge, Text } from "@chakra-ui/react";
+// import { Box, Center, Image, Flex, Badge, Text } from "@chakra-ui/react";
 
 import './Global.css';
 import Image1 from './static/image1.jpg';
